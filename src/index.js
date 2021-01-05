b46faf085c041728d947d10972a0f39d474ecdcd
Eva Keller <eva.keller1@hotmail.com>
2021-01-05 19:11:36 +0100

    create first component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList(){
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (<article>
+    <Image></Image>
+    <Title />
+    <Author />
+  </article>
+  );
+};
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71Ctz4sO6KL._AC_UL200_SR200,200_.jpg" alt="pic"/>
+
+const Title = () => <h1>Keep Sharp</h1>;
+
+const Author = () => <h4>Sanjay Gupta Dr</h4>
+
+ReactDom.render(<BookList />, document.getElementById('root'));
+
