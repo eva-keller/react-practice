81da1ba022cb7574ba833c712212bf956fb5ac8d
Eva Keller <eva.keller1@hotmail.com>
2021-01-07 16:51:52 +0100

    render lists

--- src/index.js
+++ src/index.js
@@ -1,29 +1,75 @@
 import React from'react'
 import ReactDom from 'react-dom'
+//CSS
+import './index.css'
+//setup vars
+const books = [
+{ id: 1,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wRjbln9eL._AC_UL200_SR200,200_.jpg',
+  title: 'Light of the Jedi',
+  author: 'Luke Skywalker'
+},
+{ id: 2,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71Ctz4sO6KL._AC_UL200_SR200,200_.jpg',
+  title: 'Build better brain',
+  author: 'Sanjay Gupta DR'
+},
+{ id:3,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg',
+  title: 'Atomic Habits',
+  author: 'James Clear'
+}
+];
 
 
-function BookList(){
+
+function BookList() {
   return (
-    <section>
-      <Book />
+    <section className="booklist">
+      {books.map((book) => {
+        // const {img, title,author} = book;
+        return (
+          <Book key={book.id} {...book}></Book>
+        );
+      })}
     </section>
-  );
-};
 
-const Book = () => {
-  return (<article>
-    <Image></Image>
-    <Title />
-    <Author />
+  );
+}
+
+//before refactoring
+// function BookList(){
+//   return (
+//     <section className='booklist'>
+//       <Book img={firstBook.img} 
+//             title={firstBook.title}
+//             author={firstBook.author}
+//       >
+     
+//       </Book>
+//       <Book img={secondBook.img} 
+//             title={secondBook.title}
+//             author={secondBook.author}/>
+//     </section>
+//   );
+// };
+
+
+//(props) = ({img, title, author}) is another way to declare props
+
+const Book = (props) => {
+  const {img, title, author} = props;
+  return (
+  <article className='book'>
+    <img src={img} alt=''/>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+     
   </article>
   );
 };
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71Ctz4sO6KL._AC_UL200_SR200,200_.jpg" alt="pic"/>
 
-const Title = () => <h1>Keep Sharp</h1>;
-
-const Author = () => <h4>Sanjay Gupta Dr</h4>
 
 ReactDom.render(<BookList />, document.getElementById('root'));
 
